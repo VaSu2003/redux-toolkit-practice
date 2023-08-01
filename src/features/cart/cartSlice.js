f09c028db9862fd523159b0f3baca39ab1a7a3a5
Vaibhav <vaibhav.shukla98871174@gmail.com>
2023-08-01 16:54:07 +0530

    add removeItem, increase amount, decrease amount actions in redux store

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import cartItems from "../../cartItems";
 
 const initialState = {
-    cartItems : [],
-    amount : 0,
+    cartItems : cartItems,
+    amount : 1,
     total : 0,
     isLoading: true
 
@@ -11,9 +11,29 @@ const initialState = {
 
 const cartSlice = createSlice({
     name:'cart',
-    initialState
+    initialState,
+    reducers:{
+        clearCart : (store)=>{
+            store.cartItems = []
+        },
+        removeItem : (store,action) => {
+            const itemId = action.payload
+            store.cartItems = store.cartItems.filter((item)=>item.id !== itemId)
+        },
+        increase : (store,{payload}) =>{
+            const cartItem = store.cartItems.find((item)=> item.id === payload.id);
+            // console.log(store.cartItems)
+            cartItem.amount = cartItem.amount + 1;
+        },
+        decrease : (store,{payload}) =>{
+            const cartItem = store.cartItems.find((item)=> item.id === payload.id);
+            cartItem.amount = cartItem.amount - 1;
+        }
+    }
 }) 
 
 // console.log(cartSlice)
+
+export const {clearCart,removeItem,increase,decrease} = cartSlice.actions;
 
 export default cartSlice.reducer;
